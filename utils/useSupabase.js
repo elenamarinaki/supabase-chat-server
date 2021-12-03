7b28adbd1644327e508586d47ad3a41545121a3a
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-03 02:31:11 +0000

    custom supabase hook that informs the session

--- utils/useSupabase.js
+++ utils/useSupabase.js
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import { useState } from 'react';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -7,4 +8,13 @@ const supabase = createClient(
 
 const useSupabase = () => {
   const [session, setSession] = useState(supabase.auth.session());
+
+  //   anytime a change in login happens, we update the session state and we return it along with the supabase client
+  //   argument used with '-' is not used
+  supabase.auth.onAuthStateChange(async (_event, session) => {
+    setSession(session);
+  });
+  return { session, supabase };
 };
+
+export default useSupabase;
