59aa147fcf61677c12868c40f176199aae5dcb11
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-03 02:25:36 +0000

    we have a session that is based on our supabase session - contains info about if our user is logged in, ids etc.

--- utils/useSupabase.js
+++ utils/useSupabase.js
@@ -4,3 +4,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_API_KEY
 );
+
+const useSupabase = () => {
+  const [session, setSession] = useState(supabase.auth.session());
+};
