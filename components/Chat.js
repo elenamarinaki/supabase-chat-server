8ffb72df22895b0590f48cbd641a9a4941ea0cf9
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-06 00:32:06 +0000

    displaying all messages in the console

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Chat = ({ supabase }) => {
   console.log('SUPABASE', supabase);
+
+  //   the useEffect is synchronous to prevent race conditions
+  // this is why it's getting underlined here - but it works fine ;)
+  useEffect(async () => {
+    const getMessages = async () => {
+      let { data: messages, error } = await supabase
+        .from('message')
+        .select('*');
+      console.log('MESSAGES', messages);
+    };
+    await getMessages();
+  }, []);
+
   return <div></div>;
 };
 
