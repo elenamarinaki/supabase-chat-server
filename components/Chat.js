a62c0ad76f5b424393d297cddc9289ddf544708f
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-06 00:38:55 +0000

    messages are being rendered to screen

--- components/Chat.js
+++ components/Chat.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 const Chat = ({ supabase }) => {
   console.log('SUPABASE', supabase);
 
+  const [messages, setMessages] = useState([]);
+
   //   the useEffect is synchronous to prevent race conditions
   // this is why it's getting underlined here - but it works fine ;)
   useEffect(async () => {
@@ -12,11 +14,18 @@ const Chat = ({ supabase }) => {
         .from('message')
         .select('*');
       console.log('MESSAGES', messages);
+      setMessages(messages);
     };
     await getMessages();
-  }, []);
+  }, [messages]);
 
-  return <div></div>;
+  return (
+    <div>
+      {messages.map((message) => (
+        <div key={message.id}>{message.content}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Chat;
